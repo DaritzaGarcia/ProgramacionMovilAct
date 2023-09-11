1e4ec8cc7c74e0b744540ff7f2165434cdf169d6
DaritzaGarcia <144637541+DaritzaGarcia@users.noreply.github.com>
2023-09-10 19:22:57 -0600

    Create HelloUpiicsa.kt

---
+++ HelloUpiicsa.kt
@@ -0,0 +1,24 @@
+/*PROGRAMACION MOVIL*/
+/*ALTERNATIVA ONLINE PARA TESTEO RAPIDO*/
+/*Garcia Arellano Daritza Guadalupe 6NM61*/
+
+/*En esta consola haremos lo siguiente:
+Imprimir el mismo mensaje que imprimimos en la practica "HelloUPIICSA"*/
+
+fun main() {
+    print("HelloUPIICSA")
+    
+/*Abajo de la linea de código de la tarea 
+Poner otro mensaje en consola*/
+
+    print(", Unidad Profesional Interdisciplinaria de Ingeniería y Ciencias Sociales y Administrativas\n")
+        
+/*Ahora utiliza println e imprime alguna otra cosa*/
+	println("Esta Unidad Académica pertenece al Instituto Politécnico Nacional")
+  
+ /*¿Qué pasa con los dos mensajes?*/
+ /*¿Qué diferencia hay con print?*/
+ 	print("El primer print se pondrá seguido al texto anterior por no tener salto de linea\n")
+    println("La difencia en println es que permite dar un salto de linea sin '\\n'")
+    
+}
