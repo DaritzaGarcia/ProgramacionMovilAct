7e1b343dc0471f1b479b1baaeb37ccece385f568
DaritzaGarcia <144637541+DaritzaGarcia@users.noreply.github.com>
2023-10-03 16:27:16 -0600

    Create Ejclase_Nummutable.kt

---
+++ Ejclase_Nummutable.kt
@@ -0,0 +1,6 @@
+/*6NM61 GARCIA ARELLANO DARITZA GUADALUPE EJ EN CLASE NUM MUTABLE*/
+fun main() {
+    val numSet: Set<Int> = setOf(0, 1, 2, 3, 4, 5)
+    val reverseNumSet: Set<Int> = setOf(5, 4, 3, 2, 1, 0)
+    val names: MutableSet<String> = mutableSetOf("Nombre 1", "Nombre 2")
+}
