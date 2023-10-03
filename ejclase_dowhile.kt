ac31df4e8d27557f2fb6f4c0ab7ba562ba6a0b9b
DaritzaGarcia <144637541+DaritzaGarcia@users.noreply.github.com>
2023-10-03 16:04:11 -0600

    Create ejclase_dowhile.kt

---
+++ ejclase_dowhile.kt
@@ -0,0 +1,9 @@
+/*6NM61 GARCIA ARELLANO DARITZA GUADALUPE EJ EN CLASE DOWHILE CONTADOR*/
+
+fun main (){
+    var cont = 0
+    do {
+        println("Cont: $cont")
+        cont++
+    } while (cont < 10)
+}
