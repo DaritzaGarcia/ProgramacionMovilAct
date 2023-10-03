23c7b6016d34c1e323df57108c282b92e813b13e
DaritzaGarcia <144637541+DaritzaGarcia@users.noreply.github.com>
2023-10-03 16:07:52 -0600

    Create Ejclase_areacirculo.kt

---
+++ Ejclase_areacirculo.kt
@@ -0,0 +1,13 @@
+/*6NM61 GARCIA ARELLANO DARITZA GUADALUPE EJ EN CLASE AREA DEL CIRCULO*/
+
+
+import kotlin.math.PI
+
+fun main() {
+    
+    val radio = 1.0
+    val area = PI * radio * radio
+
+    
+    println("El área del círculo con radio $radio es: $area")
+}
